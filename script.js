50a4bcdcfe325c34648d5778e69c077c12e0048c
primezm <72418219+dwzzn@users.noreply.github.com>
2025-06-26 13:49:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.querySelector('.toggle-theme').addEventListener('click', () => {
   document.body.classList.toggle('dark');
 });
+const statusEmoji = {
+  online: "online",
+  idle: "sleeping",
+  dnd: "dnd",
+  offline: "offline",
+};
+statusElement.textContent = "couldnt load it";
